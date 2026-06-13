1623621373c93d1b25698e217641575e8d3237a0
yasminemahfoud <yasminemahfoud6@gmail.com>
2026-06-13 23:35:47 +0200

    features:ajouter les fonction de show updated deleted pour chaque produis

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -1,58 +1,144 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Product;
 use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use symfony\component\Filesystem\Filesystem;
 
 final class ProductController extends AbstractController
 {
-      private $productRepository;
-      private $entityManager;
+    private ProductRepository $productRepository;
+    private $entityManager;
 
-      public function __construct(ProductRepository $productRepository,ManagerRegistry $doctrine)
-
-      {
+    public function __construct(
+        ProductRepository $productRepository,
+        ManagerRegistry $doctrine
+    ) {
         $this->productRepository = $productRepository;
         $this->entityManager = $doctrine->getManager();
-      }
-     #[Route('/product', name: 'product_list')]
-      public function index(): Response
-      {
+    }
+
+    #[Route('/product', name: 'product_list')]
+    public function index(): Response
+    {
+      
+        $products = $this->productRepository->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'productController',
+            'products' => $products,
         ]);
-      }
-  
-  #[Route('/store/product', name: 'product_store')]
+    }
+
+    #[Route('/store/product', name: 'product_store')]
     public function store(Request $request): Response
     {
         $product = new Product();
-        $form = $this->createForm(ProductType::class,$product);
+
+        $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isvalid()){
-             $product = $form->getData();
-             if($request->files->get('product')['image']){
-                $image = $request->files->get('product')['image'];
-                $image_name = time().'_'.$image->getClientOriginalName();
-                $image->move($this->getParameter('image_directory',$image_name));
-                $product->setImage($image_name);
-             }
-             $this->entityManager->persist($product);
-             $this->entityManager->flush();
-             $this->addFash(
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $image = $form->get('image')->getData();
+
+            if ($image) {
+                $imageName = time() . '_' . $image->getClientOriginalName();
+
+                $image->move(
+                    $this->getParameter('image_directory'),
+                    $imageName
+                );
+
+                $product->setImage($imageName);
+            }
+
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+
+            $this->addFlash(
                 'success',
-                'your prodect was saved'
-             );
-             return $this->redirctToRoute('product_list');
+                'Your product was saved successfully.'
+            );
+
+            return $this->redirectToRoute('product_list');
         }
+
         return $this->render('product/create.html.twig', [
-    'form' => $form->createView(),
-]);
-      
+            'form' => $form->createView(),
+        ]);
     }
+     #[Route('/product/details/{id}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+
+
+        return $this->render('product/show.html.twig', [
+            'products' => $product,
+        ]);
+    
+}
+#[Route('/product/edit/{id}', name: 'product_edit')]
+    public function editproduct(Product $product,Request $request): Response
+    {
+
+    
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $image = $form->get('image')->getData();
+
+            if ($image) {
+                $imageName = time() . '_' . $image->getClientOriginalName();
+
+                $image->move(
+                    $this->getParameter('image_directory'),
+                    $imageName
+                );
+
+                $product->setImage($imageName);
+            }
+
+            $this->entityManager->persist($product);
+            $this->entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your product updated.'
+            );
+
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('product/e.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/product/delete/{id}', name: 'product_delete')]
+    public function delete(Product $product): Response
+    {
+        $filesystem = new Filesystem();
+        $imagePath = '.uploads/'.$product->getImage();
+        if($filesystem->exists($imagePath)){
+            $filesystem->remove($imagePath);
+        }
+        $this->entityManager->remove($product);
+            $this->entityManager->flush();
+              $this->addFlash(
+                'success',
+                'Your product removed.'
+            );
+
+        return $this->redirectToRoute('product_list');
+}
+
 }
