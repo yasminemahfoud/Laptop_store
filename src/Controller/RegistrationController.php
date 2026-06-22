ce3dbe84d54766c924c85798b80ec4a9354d35a8
yasminemahfoud <yasminemahfoud6@gmail.com>
2026-06-22 22:54:44 +0200

    refactor:ajouter condition quand un persone fait la connexion il va entre directement au home

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,9 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+         if(!$this->getUser()){
+          return $this->redirectToRoute('home');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
